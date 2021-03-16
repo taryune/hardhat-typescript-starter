d5e69f85aa1164764c62a291111646a414b3a22b
taryune <tarumi@arshub.io>
2021-03-16 18:46:08 +0900

    edit config

--- scripts/sample-script.ts
+++ scripts/sample-script.ts
@@ -1,11 +1,10 @@
-import {ethers} from "hardhat";
+import { ethers } from "hardhat";
 
 async function main() {
   const Greeter = await ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, HardHat !");
 
   await greeter.deployed();
-
   console.log("Greeter deployed to:", greeter.address);
 }
 
