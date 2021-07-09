80d9f3359ea01cd521659f038af1da6342cfdb14
taryune <taryunelab@gmail.com>
2021-07-09 17:03:48 +0900

    format

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config';
-import '@typechain/hardhat'
+import '@typechain/hardhat';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 
